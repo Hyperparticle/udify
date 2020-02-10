11aa803df9db9f541ac64cbab012d6f09f4253cb
Yohei Tamura <tamuhey@gmail.com>
2020-02-11 01:26:29 +0900

    add ci

--- tests/test_import.py
+++
@@ -1,4 +0,0 @@
-from allennlp.common.util import import_submodules
-
-def test_import():
-    import_submodules("udify")
---
+++ tests/test_main.py
@@ -0,0 +1,37 @@
+from typing import Dict, List
+from allennlp.common.util import import_submodules
+import torch
+import pytest
+from allennlp.data import DatasetReader
+from allennlp.models.archival import load_archive
+from pathlib import Path
+from udify.models.udify_model import OUTPUTS as UdifyOUTPUTS  # type: ignore
+
+
+def test_import():
+    import_submodules("udify")
+
+
+@pytest.fixture(scope="session")
+def model():
+    archive = load_archive(
+        str((Path(__file__).parent / ".." / "data" / "archive").absolute())
+    )
+    config = archive.config
+    dataset_reader_params = config["dataset_reader"]
+    dataset_reader = DatasetReader.from_params(dataset_reader_params)
+
+    def proc(tokens_list: List[List[str]]) -> Dict:
+        archive.model.eval()
+        with torch.no_grad():
+            instances = [
+                dataset_reader.text_to_instance(tokens) for tokens in tokens_list
+            ]
+            outputs = archive.model.forward_on_instances(instances)
+        return outputs
+
+    return proc
+
+
+def test_call(model):
+    outputs = model([["Who", "are", "you", "?"]])
